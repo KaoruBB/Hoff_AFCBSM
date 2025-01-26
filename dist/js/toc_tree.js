d680aca67a02f8286838f5407fc63de1baba8b78
KaoruBB <kaorugmbl1234@gmail.com>
2025-01-26 17:05:13 +0900

    initial commit

---
+++ dist/js/toc_tree.js
@@ -0,0 +1,43 @@
+const tocTree = [
+	{
+		"name": "Answers of exercises on Hoff, A first course in Bayesian statistical methods", 
+		"index": "0", 
+		"parent": "Answers of exercises on Hoff, A first course in Bayesian statistical methods", 
+		"file": "./index.html"
+	},
+	{
+		"name": "setup :ignore:", 
+		"index": "1", 
+		"parent": "Answers of exercises on Hoff, A first course in Bayesian statistical methods", 
+		"file": "./index.html"
+	},
+	{
+		"name": "About This Page :onlyforsite:English:", 
+		"index": "2", 
+		"parent": "Answers of exercises on Hoff, A first course in Bayesian statistical methods", 
+		"file": "./index.html"
+	},
+	{
+		"name": "Parts :ignore:", 
+		"index": "3", 
+		"parent": "Answers of exercises on Hoff, A first course in Bayesian statistical methods", 
+		"file": "./index.html"
+	},	{
+		"name": "標準ベイズ統計学の演習問題の解答例", 
+		"index": "0", 
+		"parent": "標準ベイズ統計学の演習問題の解答例", 
+		"file": "./index_jp.html"
+	},
+	{
+		"name": "setup :ignore:", 
+		"index": "1", 
+		"parent": "標準ベイズ統計学の演習問題の解答例", 
+		"file": "./index_jp.html"
+	},
+	{
+		"name": "このサイトについて :onlyforsite:Japanese:", 
+		"index": "2", 
+		"parent": "標準ベイズ統計学の演習問題の解答例", 
+		"file": "./index_jp.html"
+	},
+];
